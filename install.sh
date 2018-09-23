d73be1a00736caf0710b7d0203b119bc4962b3b3
Donald Cheung <donald@localhost.localdomain>
2018-09-24 01:59:55 +0800

    sh install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+mkdir ~/netease-cloud-music
+cd ~/netease-cloud-music
+wget http://d1.music.126.net/dmusic/netease-cloud-music_1.1.0_amd64_ubuntu.deb
+ar -xvf netease-cloud-music_1.1.0_amd64_ubuntu.deb
+tar -xvf data.tar.xz
+sudo cp -r usr/* /usr/
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install vlc
+cd ~
+rm -rf ~/Netease-Cloud-Music
